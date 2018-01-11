03106dc8e79d8695194cb34fc1121d29ea554d39
prerak21 <prerak211192@gmail.com>
2018-01-11 11:40:50 -0600

    Update WebDriverSetup.java

--- src/testSetup/WebDriverSetup.java
+++ src/testSetup/WebDriverSetup.java
@@ -1,19 +1,25 @@
 package testSetup;
 
-import org.testng.annotations.*;
 import org.openqa.selenium.WebDriver;
-
-import org.openqa.selenium.chrome.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+
+import java.io.File;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by prerak thakkar
  */
 public class WebDriverSetup {
 
-    private WebDriver driver;
+    private static  WebDriver driver;
 
-    static String driverPath = "lib/chromedriver.exe";
+    //static String driverPath = "lib/chromedriver.exe";
+    static String geckoDriverPath = "lib/geckodriver.exe";
 
 
     public WebDriver getDriver() {
@@ -24,20 +30,50 @@ public class WebDriverSetup {
 
         if(browserType.equalsIgnoreCase("chrome")){
 
-            driver = launchChrome(URL);
+         driver = launchChrome(URL);
 
-        } 
+
+        } else if (browserType.equalsIgnoreCase("firefox")){
+
+            driver = launchFirefox(URL);
+        }
+    }
+
+    private static WebDriver launchFirefox(String URL){
+
+        System.out.println("Launch Firefox browser");
+
+        System.setProperty("webdriver.gecko.driver", geckoDriverPath);
+        driver = new FirefoxDriver();
+        driver.get(URL);
+        System.out.println(URL);
+        return driver;
     }
 
     private static WebDriver launchChrome(String URL ){
 
         System.out.println("Launch Chrome browser");
 
-        System.setProperty("webdriver.chrome.driver", driverPath);
 
-        WebDriver driver = new ChromeDriver();
+
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+
+
+
+
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("test-type");
+        options.addArguments("disable-extensions");
+        options.addArguments("start-maximized");
+        options.addArguments("disable-infobars");
+        options.addArguments("disable-notifications");
+        options.setAcceptInsecureCerts(true);
+
+         driver = new ChromeDriver(options);
 
         driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
 
         driver.navigate().to(URL);
 
