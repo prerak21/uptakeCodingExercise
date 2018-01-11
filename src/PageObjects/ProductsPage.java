301b8f259cbe972b2966942c8033acb0aad08102
prerak21 <prerak211192@gmail.com>
2018-01-11 11:38:59 -0600

    Update ProductsPage.java

--- src/PageObjects/ProductsPage.java
+++ src/PageObjects/ProductsPage.java
@@ -3,6 +3,9 @@ package PageObjects;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import testSetup.WebDriverSetup;
 
 /**
@@ -12,48 +15,54 @@ public class ProductsPage extends WebDriverSetup{
 
     protected WebDriver driver;
 
-    String currentURL = driver.getCurrentUrl();
+
 
     public ProductsPage(WebDriver driver){
 
         this.driver = driver;
+        PageFactory.initElements(this.driver,this);
     }
 
     @FindBy(css="div[class='l-site-header__brand'] a")
-    WebElement homeButton;
+    private WebElement homeButton;
+
+    @FindBy(css="div[class='l-site__header']")
+    private WebElement siteContent;
 
     @FindBy(css="div[class='l-hero-header__content'] a")
-    WebElement getDemoButton;
+    private WebElement getDemoButton;
 
     @FindBy(css="div[class='l-hero-header__content'] h1")
-    WebElement productsHeader;
+    private WebElement productsHeader;
 
     @FindBy(css="div[class='l-form-container ']")
-    WebElement demoForm;
+    private WebElement demoForm;
 
 
 
-    public WebElement getHomeButton(){
-
-        return homeButton;
-    }
 
     public boolean verifyProductsPage(){
 
         System.out.println("Verifying Products link Navigation to correct page ");
+
+        String currentURL = driver.getCurrentUrl();
 
         return currentURL.contains("products");
     }
 
 
 
-    public ProductsPage clickDemoLink(){
+    public ProductsPage clickDemoLink() throws Exception{
 
         System.out.println("Clicking on Get A Demo button under Products");
 
         if(getDemoButton.isDisplayed() || getDemoButton.isEnabled()){
 
             getDemoButton.click();
+
+            WebDriverWait wait = new WebDriverWait(driver, 2);
+
+            wait.until(ExpectedConditions.visibilityOf(demoForm));
         } else {
 
             System.out.println("Get A Demo Button not found");
@@ -67,6 +76,23 @@ public class ProductsPage extends WebDriverSetup{
         System.out.println("Verifying Demo Request Form is Displayed");
 
         return demoForm.isDisplayed();
+    }
+
+    public HomePage clickHomeButton(){
+        System.out.println("Navigating to HomePage");
+
+        siteContent.click();
+
+        if(homeButton.isDisplayed() ||  homeButton.isEnabled()){
+
+            homeButton.click();
+        } else {
+
+            System.out.println("Home Button not found");
+        }
+
+        return new HomePage(driver);
+
     }
 
 
